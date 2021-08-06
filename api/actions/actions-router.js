610ec32241afa1946e7e9c34cf3736ce4dc800ad
Bobby <monkeyeden.rjt@gmail.com>
2021-08-06 17:23:12 -0400

    CheckPoint

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -1,1 +1,79 @@
 // Write your "actions" router here!
+const express = require('express');
+
+const Action = require("./actions-model")
+
+const router= express.Router()
+
+//GET
+router.get('/',async (req,res)=>{
+    const actions = await Action.get()
+    res.json(actions)
+})
+//Get with ID
+router.get('/:id',async(req,res)=>{
+    const{id}=req.params
+    try{
+        const action= await Action.get(id);
+        if(!action){
+            res.status(404).json({message:"This is not an ID"})
+        }else{
+            res.json(action)
+        }
+    }catch{
+        res.status(500).json({message:"ERROR NOOOO!!!"})
+    }    
+})
+//POST
+router.post('/',async (req,res)=>{
+    const {description,notes} = req.body;
+    try{
+        if(!notes || !description){
+            res.status(400).json({message:'please provide notes and description for the project'})
+        }else{
+            console.log(req.body)
+            const actions = await Action.insert(req.body)
+            res.status(200).json(actions)
+        }
+    }catch(err){
+        res.status(500).json({message:"no server"})
+    }
+})
+//PUT ID
+router.put('/:id',async(req,res)=>{
+    const {id} = req.params
+    const {description,notes,project_id} = req.body;
+    try{
+        const action= await Action.get(id);
+        if(!notes || !description || !project_id ){
+            res.status(400).json({message:'please provide notes, description for the project'})
+        }else{
+            if(!action){
+                res.status(404).json({message:"This is not an ID"})
+            }else{ 
+                const updatedAction = await Action.update(id,req.body)
+                res.json(updatedAction)
+            }
+        }
+    }catch(err){
+        res.status(500).json({message:"no server"})
+    }
+})
+
+router.delete('/:id', async (req,res)=>{
+    const {id} = req.params
+    const action= await Action.get(id)
+    try{
+        if(!action){
+            res.status(404).json({message:"could not find that ID "})
+        }else{
+            await Action.remove(req.params.id)
+            res.json()
+        }
+    }catch(err){
+        res.status(500).json({message:"Mean Error"})
+    }
+})
+
+
+module.exports = router
--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,1 +1,95 @@
 // Write your "projects" router here!
+const express = require('express');
+
+const Projects = require("./projects-model")
+
+const router= express.Router()
+//GET
+router.get('/',(req,res)=>{
+   Projects.get()
+   .then(projects=>{
+       res.status(200).json(projects)
+   })
+})
+//GET with ID
+router.get('/:id', async (req,res)=>{
+    const{id}=req.params
+    try{
+        const project= await Projects.get(id)
+        if(!project){
+            res.status(404).json({message:"This is not an ID"})
+        }else{
+            res.json(project)
+        }
+    }catch{
+        res.status(500).json({message:"ERROR NOOOO!!!"})
+    }    
+})
+//POST
+router.post('/',async(req,res)=>{
+    const {name,description} = req.body
+    try{
+    if(!name || !description){
+        res.status(400).json({message:'please provide name and description for the project'})
+    }else{
+        const project = await Projects.insert(req.body)
+        res.status(200).json(project)
+    }
+    }catch(err){
+        res.status(500).json({message:"no server"})
+    }
+})
+//PUT ID -No clue why this is failing 
+router.put('/:id',async (req,res)=>{
+    const{id}=req.params
+    const {name,description,completed} = req.body
+    try{
+        const project= await Projects.get(id)
+        if(!name || !description || !completed){
+            res.status(400).json({message:'please provide name,description and if its completed for the project'})
+        }else{
+            if(!project){
+                res.status(404).json({message:"could not find that ID "})
+            }else{
+                const updatedProject = await Projects.update(id,req.body)
+                res.json(updatedProject)
+            }
+        }
+
+    }catch(err){
+        res.status(500).json({message:"Mean Error"})
+    }
+})
+
+//DELETE id
+router.delete('/:id', async (req,res)=>{
+    const {id} = req.params
+    const project= await Projects.get(id)
+    try{
+        if(!project){
+            res.status(404).json({message:"could not find that ID "})
+        }else{
+            await Projects.remove(req.params.id)
+            res.json()
+        }
+    }catch(err){
+        res.status(500).json({message:"Mean Error"})
+    }
+})
+router.get("/:id/actions",async(req,res)=>{
+    const {id}=req.params
+    try{
+        const project = await Projects.get(id)
+        if(!project){
+            res.status(404).json({message:"could not find that ID "})
+        }else{
+            const actions = await Projects.getProjectActions(id)
+            res.json(actions)
+        }
+    }catch(err){
+        res.status(500).json({message:"Mean Error"})
+    }
+
+})
+
+module.exports = router
